3221a38bfec55885a63f2c8974330689cb0564a6
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2024-09-21 19:17:03 +0545

    updated controllers

--- backend_express/src/modules/course/controllers/get.controller.js
+++ backend_express/src/modules/course/controllers/get.controller.js
@@ -1,29 +1,33 @@
-import { Course } from "../models/course.model.js";
+import { Course } from '../models/course.model.js';
 export async function getCourses(req, res) {
-  try {
-    const courses = await Course.find().populate("created_by");
-    return res.status(200).json(courses);
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
-  }
+	try {
+		const courses = await Course.find()
+			.populate('created_by')
+			.populate('video_id');
+		return res.status(200).json(courses);
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: 'Internal Server Error', error: error.message });
+	}
 }
 
 // Function to get a course by ID
 export async function getCourseById(req, res) {
-  try {
-    const courseId = req.params.id;
-    const course = await Course.findById(courseId).populate("created_by");
+	try {
+		const courseId = req.params.id;
+		const course = await Course.findById(courseId)
+			.populate('created_by')
+			.populate('video_id');
 
-    if (!course) {
-      return res.status(404).json({ message: "Course not found." });
-    }
+		if (!course) {
+			return res.status(404).json({ message: 'Course not found.' });
+		}
 
-    return res.status(200).json(course);
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
-  }
+		return res.status(200).json(course);
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ message: 'Internal Server Error', error: error.message });
+	}
 }
