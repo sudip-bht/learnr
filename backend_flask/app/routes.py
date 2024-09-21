406a97061949a1094160b7fd5a5686317ff3430e
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2024-09-21 19:18:00 +0545

    removed unnecessary routes

--- backend_flask/app/routes.py
+++ backend_flask/app/routes.py
@@ -1,29 +1,31 @@
 # app/routes.py
-from flask import  request, jsonify
+from flask import request, jsonify
 import os
 
 from .controllers.question_controller import get_answer, initialize_qa_chain, load_youtube_transcript
 from .controllers.quiz_controller import create_flashcards, generate_quiz_from_transcript
-from .controllers.courser_controller import create_course,edit_course,delete_course
+from .controllers.courser_controller import create_course, edit_course, delete_course
 from .controllers.quiz_controller import generate_quiz_from_transcript
 from .controllers.video_controller import get_youtube_playlist
-from .controllers.rag_controller import ask_question,process_pdf
-from .controllers.audio_controller import create_audio,edit_audio,delete_audio,get_audios_by_video
+from .controllers.rag_controller import ask_question, process_pdf
+from .controllers.audio_controller import create_audio, edit_audio, delete_audio, get_audios_by_video
 from .controllers.image_controller import generate_image_url
 from app import app
+
+
 @app.route('/',  methods=['GET'])
 def index():
     return 'Hello its working'
+
 
 @app.route('/ask', methods=['POST'])
 def ask():
     return ask_question(request)
 
+
 @app.route('/upload_pdf', methods=['POST'])
 def upload_pdf():
     return process_pdf(request)
-
-
 
 
 # Course routes
@@ -31,38 +33,42 @@ def upload_pdf():
 def create_course_route():
     return create_course(request)
 
+
 @app.route('/courses/<course_id>', methods=['PATHC'])
 def edit_course_route(course_id):
-    return edit_course(course_id,request)
+    return edit_course(course_id, request)
+
 
 @app.route('/courses/<course_id>', methods=['DELETE'])
-def delete_course_route(course_id,request):
+def delete_course_route(course_id, request):
     return delete_course(course_id)
 
 
-
-@app.route('/getPlaylist' ,methods=['POST'])
+@app.route('/getPlaylist', methods=['POST'])
 def extractPlaylist():
-    data= request.get_json()
+    data = request.get_json()
     return get_youtube_playlist(data['url'])
-
 
 
 @app.route('/audio', methods=['POST'])
 def add_audio():
     return create_audio(request)
 
+
 @app.route('/audio/<audio_id>', methods=['PUT'])
 def update_audio(audio_id):
-    return edit_audio(audio_id,request)
+    return edit_audio(audio_id, request)
+
 
 @app.route('/audio/<audio_id>', methods=['DELETE'])
 def remove_audio(audio_id):
     return delete_audio(audio_id)
 
+
 @app.route('/video/<video_id>/audios', methods=['GET'])
 def get_audios(video_id):
     return get_audios_by_video(video_id)
+
 
 @app.route('/ask_question', methods=['POST'])
 def ask_question():
@@ -84,6 +90,7 @@ def ask_question():
 
     return jsonify(response)
 
+
 @app.route('/generate_quiz_with_flashcards', methods=['POST'])
 def generate_quiz_with_flashcards():
     data = request.json
@@ -96,7 +103,8 @@ def generate_quiz_with_flashcards():
     full_transcript = " ".join([doc.page_content for doc in documents])
     qa_chain = initialize_qa_chain(video_url)
 
-    quiz = generate_quiz_from_transcript(full_transcript, qa_chain, num_questions)
+    quiz = generate_quiz_from_transcript(
+        full_transcript, qa_chain, num_questions)
     flashcards = create_flashcards(quiz, days)
 
     response = {
@@ -106,8 +114,8 @@ def generate_quiz_with_flashcards():
 
     return jsonify(response)
 
-@app.route('/generateimage/<title>',methods=['GET'])
-def generate_image(title):
-    prompt="generate image for course title"+title
-    return generate_image_url(prompt=prompt)
 
+@app.route('/generateimage/<title>', methods=['GET'])
+def generate_image(title):
+    prompt = "generate image for course title"+title
+    return generate_image_url(prompt=prompt)
